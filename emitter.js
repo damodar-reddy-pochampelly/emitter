4f086413be62ea244b3aa64c530dafd61a96b6df
damodarreddy <damodarreddy18107@gmail.com>
2023-09-11 03:01:19 +0000

    changed code in emitter.js

--- emitter.js
+++ emitter.js
@@ -9,13 +9,6 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Function to generate a SHA-256 hash
-function generateHash(data) {
-  const hash = crypto.createHash("sha256");
-  hash.update(data);
-  return hash.digest("hex");
-}
-
 const socket = io.connect("https://timerseries.onrender.com", {
   transports: ["websocket"], // Use only WebSocket transport
 });
@@ -38,23 +31,39 @@ function emitEncryptedMessages() {
       destination,
     };
 
-    const secret_key = generateHash(JSON.stringify(originalMessage));
+    // Generate a secret key as a SHA-256 hash of the message
+    const secret_key = crypto
+      .createHash("sha256")
+      .update(JSON.stringify(originalMessage))
+      .digest("hex");
 
-    const encryptionKey = crypto.randomBytes(32); // Generate a 32-byte encryption key
-    const iv = crypto.randomBytes(16); // Generate a random IV
+    // Create an encryption key and IV for AES-GCM
+    const encryptionKey = crypto.randomBytes(32); // 32 bytes for AES-256
+    const iv = crypto.randomBytes(12); // 12 bytes for AES-GCM IV
 
-    const cipher = crypto.createCipheriv("aes-256-ctr", encryptionKey, iv);
+    // Create an AES-GCM cipher
+    const cipher = crypto.createCipheriv("aes-256-gcm", encryptionKey, iv);
+
+    // Encrypt the message and get the authentication tag
     let encryptedMessage = cipher.update(
-      JSON.stringify({ ...originalMessage, secret_key }),
+      JSON.stringify(originalMessage),
       "utf8",
       "hex"
     );
     encryptedMessage += cipher.final("hex");
+    const authTag = cipher.getAuthTag().toString("hex");
 
-    messages.push(encryptedMessage);
+    // Combine IV, encrypted message, and authentication tag in a specific order
+    const encryptedPayload = iv.toString("hex") + encryptedMessage + authTag;
+
+    // Base64 encode the payload before sending
+    const payload64 = Buffer.from(encryptedPayload, "hex").toString("base64");
+
+    messages.push(payload64);
   }
 
   const messageStream = messages.join("|");
+  console.log(messageStream);
   socket.emit("encryptedMessageStream", messageStream);
 
   setTimeout(emitEncryptedMessages, 10000); // Emit messages every 10 seconds
